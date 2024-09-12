b79a40ae1be339b63c710ecd4caf46bcc226047a
CodeCantalope <deedoleedo@gmail.com>
2024-09-13 01:55:38 +0300

    Added 'Articles' Component & API Calls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./navbar/navbar.component";
 import { HeroComponent } from "./hero/hero.component";
 import { BrowseMenuComponent } from "./browse-menu/browse-menu.component";
+import { ArticlesComponent } from "./articles/articles.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, BrowseMenuComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, BrowseMenuComponent, ArticlesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/articles/articles.component.ts
@@ -0,0 +1,58 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-articles',
+  standalone: true,
+  imports: [],
+  templateUrl: './articles.component.html',
+  styleUrl: './articles.component.css'
+})
+
+
+export class ArticlesComponent {
+  articles : Article[] = []
+
+  ngOnInit(): void {
+    fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=5&tags=under_30_minutes', {
+      method: 'GET',
+      headers: {
+        'x-rapidapi-host': 'tasty.p.rapidapi.com',
+        'x-rapidapi-key': '2abe3342b4msh5b5375d9506f7c1p1e5b55jsn0afaad8fc9aa'
+      }
+    })
+    .then(response => response.json())
+    .then(data => {
+      for (let i = 0; i < data.results.length; i++) {
+        this.articles.push(new Article(data.results[i].name, data.results[i].created_at, data.results[i].thumbnail_url, data.results[i].video_url, data.results[i].description, data.results[i].id))
+      }
+    })
+    .catch(error => console.error(error));
+  }
+}
+
+class Article {
+  title: string
+  date: string
+  thumbnail: string
+  video: string
+  description: string
+  id: number
+
+  /**
+   * Creates a new Article object
+   * @param title The title of the article
+   * @param date The date of the article
+   * @param thumbnail The thumbnail image of the article
+   * @param video The video url of the article
+   * @param description The description of the article
+   * @param id The id of the article
+   */
+  constructor(title: string, date: number, thumbnail: string, video: string, description: string, id: number) {
+    this.title = title
+    this.date = new Date(date * 1000).toLocaleString('en-US', {month: 'long', day: 'numeric', year: 'numeric'})
+    this.thumbnail = thumbnail
+    this.video = video
+    this.description = description
+    this.id = id
+  }
+}
