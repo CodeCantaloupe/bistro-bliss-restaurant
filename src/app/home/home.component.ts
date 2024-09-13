6afa4035fd25c7823a682a853926d39fb6342218
CodeCantalope <deedoleedo@gmail.com>
2024-09-14 02:46:00 +0300

    Added 'Services' Component & Fixed Navbar Links Padding Problem

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeroComponent } from "../hero/hero.component";
 import { BrowseMenuComponent } from "../browse-menu/browse-menu.component";
 import { CustomersReviewsComponent } from "../customers-reviews/customers-reviews.component";
+import { ServicesComponent } from "../services/services.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeroComponent, BrowseMenuComponent, CustomersReviewsComponent],
+  imports: [HeroComponent, BrowseMenuComponent, CustomersReviewsComponent, ServicesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
