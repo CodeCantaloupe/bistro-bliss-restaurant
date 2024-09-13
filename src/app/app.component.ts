fb60157de85cb6eb67a2790254afa3de752bc85e
CodeCantalope <deedoleedo@gmail.com>
2024-09-13 15:56:19 +0300

    Added 'Footer' Component & Added Footer Media Queries & Adjusted Hover Style for Hero Buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from "./navbar/navbar.component";
 import { HeroComponent } from "./hero/hero.component";
 import { BrowseMenuComponent } from "./browse-menu/browse-menu.component";
 import { ArticlesComponent } from "./articles/articles.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, BrowseMenuComponent, ArticlesComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, BrowseMenuComponent, ArticlesComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
